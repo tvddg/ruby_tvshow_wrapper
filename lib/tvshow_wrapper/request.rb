b149d8b31ece87e03797ee3c9886f750906df936
ddg <thewhiteless@yahoo.com>
2026-03-25 20:16:27 +0300

    Fixed TLS connection problem

--- lib/tvshow_wrapper/request.rb
+++ lib/tvshow_wrapper/request.rb
@@ -1,15 +1,21 @@
 require 'net/http'
 require 'json'
 require 'uri'
+require 'openssl'
 
 module TvshowWrapper
   class Request
-    BASE_URL = "https://api.tvmaze.com/"
+    BASE_URL = "https://api.tvmaze.com"
 
     def self.get(path, params = {})
       uri = URI(BASE_URL + path)
       uri.query = URI.encode_www_form(params) unless params.empty?
+      
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true;
 
+      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      
       response = Net::HTTP.get_response(uri)
 
       unless response.is_a?(Net::HTTPSuccess)
