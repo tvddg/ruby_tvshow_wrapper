b2debecdb911392de89514b0fe64dab001b9f29e
ddg <thewhiteless@yahoo.com>
2026-03-25 18:36:34 +0300

    Initial commit

---
+++ lib/tvshow_wrapper.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tvshow_wrapper/version"
+
+module TvshowWrapper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tvshow_wrapper/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module TvshowWrapper
+  VERSION = "0.1.0"
+end
