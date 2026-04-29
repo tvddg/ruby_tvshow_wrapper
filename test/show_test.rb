4acd07f4ce9b980e4c1b33d14cdb5326c0dfa3b3
Евгений Щетиков <shchetikovevgeniy@gmail.com>
2026-04-29 23:32:59 +0300

    Extended show model and covered with tests

--- test/show_test.rb
+++ test/show_test.rb
@@ -1,10 +1,14 @@
 require_relative 'test_helper'
 
+require_relative 'test_helper'
+
 class ShowTest < Minitest::Test
   def setup
     attrs = {
       "id" => 1,
       "name" => "Breaking Bad",
+      "summary" => "<p><b>Breaking Bad</b> follows protagonist Walter White, a chemistry teacher who lives in New Mexico with his wife and teenage son who has cerebral palsy.</p>",
+      "image" => { "medium" => "/some/url/blablabla"},
       "genres" => ["Drama"],
       "rating" => { "average" => 9.5 }
     }
@@ -14,11 +18,14 @@ class ShowTest < Minitest::Test
   def test_attributes
     assert_equal 1, @show.id
     assert_equal "Breaking Bad", @show.name
+    assert_equal "Breaking Bad follows protagonist Walter White, a chemistry teacher who lives in New Mexico with his wife and teenage son who has cerebral palsy.", @show.summary
+    assert_equal "/some/url/blablabla", @show.image
     assert_includes @show.genres, "Drama"
     assert_equal 9.5, @show.rating
   end
 
   def test_to_s
-    assert_equal "1. Breaking Bad (9.5) - Drama", @show.to_s
+    assert_equal "1. Breaking Bad (9.5) - Drama\nBreaking Bad follows protagonist Walter White, a chemistry teacher who lives in New Mexico with his wife and teenage son who has cerebral palsy.\nImage URL: /some/url/blablabla", @show.to_s
   end
 end
+﻿
