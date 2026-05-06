2216f01ef0bd65a226eaf5cb729a6a61ebf431da
ddg <thewhiteless@yahoo.com>
2026-05-06 09:06:09 +0300

    Increased image quality, edited gitignore

--- lib/tvshow_wrapper/show.rb
+++ lib/tvshow_wrapper/show.rb
@@ -1,16 +1,31 @@
 module TvshowWrapper
   class Show
-    attr_reader :id, :name, :genres, :rating
+    attr_reader :id, :name, :summary, :image, :genres, :rating
 
     def initialize(attrs)
-      @id     = attrs["id"]
-      @name   = attrs["name"]
-      @genres = attrs["genres"]
-      @rating = attrs.dig("rating", "average")
+      @id      = attrs["id"]
+      @name    = attrs["name"]
+      @summary = prepare(attrs["summary"])
+      @image   = attrs.dig("image", "original")
+      @genres  = attrs["genres"]
+      @rating  = attrs.dig("rating", "average")
     end
 
     def to_s
-      "#{id}. #{name} (#{rating || "N/A"}) #{ genres.any? ? "- #{genres.join(", ") }" : "N/A" }"
+      "#{id}. #{name} (#{rating || "N/A"}) #{ genres.any? ? "- #{genres.join(", ") }" : "N/A" }\n#{summary || "N/A"}\nImage URL: #{image || "N/A"}"
+    end
+
+    def prepare(text)
+        return nil if text.nil? || text.strip.empty?
+
+        text
+        .gsub(/<[^>]*>/, '')   
+        .gsub('&amp;', '&')
+        .gsub('&nbsp;', ' ')
+        .strip
+        .split(".")
+        .first(3)
+        .join(".") + "."
     end
   end
 end
--- test/show_test.rb
+++ test/show_test.rb
@@ -5,6 +5,8 @@ class ShowTest < Minitest::Test
     attrs = {
       "id" => 1,
       "name" => "Breaking Bad",
+      "summary" => "<p><b>Breaking Bad</b> follows protagonist Walter White, a chemistry teacher who lives in New Mexico with his wife and teenage son who has cerebral palsy.</p>",
+      "image" => { "original" => "/some/url/blablabla"},
       "genres" => ["Drama"],
       "rating" => { "average" => 9.5 }
     }
@@ -14,11 +16,13 @@ class ShowTest < Minitest::Test
   def test_attributes
     assert_equal 1, @show.id
     assert_equal "Breaking Bad", @show.name
+    assert_equal "Breaking Bad follows protagonist Walter White, a chemistry teacher who lives in New Mexico with his wife and teenage son who has cerebral palsy.", @show.summary
+    assert_equal "/some/url/blablabla", @show.image
     assert_includes @show.genres, "Drama"
     assert_equal 9.5, @show.rating
   end
 
   def test_to_s
-    assert_equal "1. Breaking Bad (9.5) - Drama", @show.to_s
+    assert_equal "1. Breaking Bad (9.5) - Drama\nBreaking Bad follows protagonist Walter White, a chemistry teacher who lives in New Mexico with his wife and teenage son who has cerebral palsy.\nImage URL: /some/url/blablabla", @show.to_s
   end
 end
